ea1a581a5c46e58aed72bd392ed2b7206fa31db5
thinun <thinun.rightmo@gmail.com>
2024-08-26 17:41:11 +0530

    error fix

--- src/app.js
+++ src/app.js
@@ -23,9 +23,9 @@ app.get('/api-scraper', async (req, res) => {
         const data = await scrapeData(url, slug, drawNo);
         res.json({ success: true, data });
     } catch (error) {
-        res.status(500).json({ success: false, error: error });
+        res.status(500).json({ success: false, error: error.message});
     }
-    console.log("hi");
+
 });
 
 module.exports = app;
