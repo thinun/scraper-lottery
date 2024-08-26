1ad49d597a4b460766ea232fa2e68d673b586804
thinun <thinun.rightmo@gmail.com>
2024-08-26 18:07:46 +0530

    add args

--- src/scraper.js
+++ src/scraper.js
@@ -3,17 +3,15 @@ const puppeteer = require('puppeteer');
 const scrapeData = async (url, slug, drawNo) => {
     const finalUrl = `${url}/${slug}/${drawNo}`;
 
-
-    const browser = await puppeteer.launch(
-        {
-            executablePath: '/usr/bin/google-chrome',
-            headless: true
-        }
-    );
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/google-chrome-stable',
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
 
     const page = await browser.newPage();
 
-    await page.goto(finalUrl, {waitUntil: 'networkidle2'});
+    await page.goto(finalUrl, { waitUntil: 'networkidle2' });
 
     const resultsHtmlArray = await page.$$eval('.B', elements =>
         elements.map(element => element.innerHTML)
