7fc007937d7aef7578f7337ddadb0d06d9416694
thinun <thinun.rightmo@gmail.com>
2024-08-26 15:35:33 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const { scrapeData } = require('./scraper');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+// Scraping API endpoint
+app.get('/api-scraper', async (req, res) => {
+    const { url, slug, drawNo } = req.query;  // Get query parameters from the request
+
+
+
+    if (!url || !slug || !drawNo) {
+        return res.status(400).json({ error: 'Missing required query parameters' });
+    }
+
+    try {
+        const data = await scrapeData(url, slug, drawNo);
+        res.json({ success: true, data });
+    } catch (error) {
+        res.status(500).json({ success: false, error: 'Failed to scrape data' });
+    }
+});
+
+module.exports = app;
---
+++ src/scraper.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+const scrapeData = async (url, slug, drawNo) => {
+    const finalUrl = `${url}/${slug}/${drawNo}`;
+
+
+
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    await page.goto(finalUrl, { waitUntil: 'networkidle2' });
+
+    const resultsHtmlArray = await page.$$eval('.B', elements =>
+        elements.map(element => element.innerHTML)
+    );
+
+    await browser.close();
+
+    return resultsHtmlArray;
+};
+
+module.exports = {
+    scrapeData
+};
