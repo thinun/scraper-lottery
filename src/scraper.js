11d9ce397d64f92868d6b660d539677bbaadda9a
thinun <thinun.rightmo@gmail.com>
2024-08-26 17:56:44 +0530

    add executablePath

--- src/scraper.js
+++ src/scraper.js
@@ -3,15 +3,17 @@ const puppeteer = require('puppeteer');
 const scrapeData = async (url, slug, drawNo) => {
     const finalUrl = `${url}/${slug}/${drawNo}`;
 
-    console.log(finalUrl)
 
-
-
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+        {
+            executablePath: '/usr/bin/google-chrome',
+            headless: true
+        }
+    );
 
     const page = await browser.newPage();
 
-    await page.goto(finalUrl, { waitUntil: 'networkidle2' });
+    await page.goto(finalUrl, {waitUntil: 'networkidle2'});
 
     const resultsHtmlArray = await page.$$eval('.B', elements =>
         elements.map(element => element.innerHTML)
